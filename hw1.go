01ecc8091f832ed470d1b47ec5ded9323cfe639a
Denis <someemail@email.ru>
2026-04-20 18:19:36 +0000

    practice func

---
+++ hw1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x, y int64
+	_, _ = fmt.Scan(&x, &y)
+	fmt.Println(sumDiv(x, y))
+	fmt.Println(oper(x,y,mult))
+}
+
+func sumDiv(x int64, y int64) (int64, int64, int64)  {
+	sum := x+y
+	div:= x/y
+	waste:= x - div
+	return sum, div, waste
+}
+
+func mult(x int64, y int64) int64 {
+	return x*y
+}
+func oper(x, y int64, operation func(int64, int64) int64) int64 {
+	return operation(x, y)
+}
