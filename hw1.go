c47b392f7979a75aeb9422a47325fe36217ca19c
Denis <someemail@email.ru>
2026-04-22 13:17:28 +0000

    add new func

--- hw1.go
+++ hw1.go
@@ -1,26 +1,26 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var x, y int64
-	_, _ = fmt.Scan(&x, &y)
-	fmt.Println(sumDiv(x, y))
-	fmt.Println(oper(x,y,mult))
-}
+// func main() {
+// 	var x, y int64
+// 	_, _ = fmt.Scan(&x, &y)
+// 	fmt.Println(sumDiv(x, y))
+// 	fmt.Println(oper(x,y,mult))
+// }
 
-func sumDiv(x int64, y int64) (int64, int64, int64)  {
-	sum := x+y
-	div:= x/y
-	waste:= x - div
-	return sum, div, waste
-}
+// func sumDiv(x int64, y int64) (int64, int64, int64)  {
+// 	sum := x+y
+// 	div:= x/y
+// 	waste:= x - div
+// 	return sum, div, waste
+// }
 
-func mult(x int64, y int64) int64 {
-	return x*y
-}
-func oper(x, y int64, operation func(int64, int64) int64) int64 {
-	return operation(x, y)
-}
+// func mult(x int64, y int64) int64 {
+// 	return x*y
+// }
+// func oper(x, y int64, operation func(int64, int64) int64) int64 {
+// 	return operation(x, y)
+// }
--- hw2.go
+++ hw2.go
@@ -6,9 +6,11 @@ import (
 type Employee struct {
 	Name string
 	Age int16
-	Positon string
+	Position string
 	Salary float64
 }
+
+var employees []Employee
 
 func main() {
 	/*
@@ -19,15 +21,36 @@ func main() {
 		Salary float64
 	}
 	*/
-	x := Employee{
+	employees = append(employees,Employee{
 		Name: "Denis",
 		Age: 28,
-		Positon: "CEO",
+		Position: "CEO",
 		Salary: 999999.99,
-	}
-	x.info()
+	})
+	employees = addEmployee(employees, "Vasya", 23, "Developer", 54334.55)
+	printAllEmployees(employees)
 }
 
 func (e Employee) info() {
-	fmt.Printf("Name: %s | Age: %d | Position: %s | Salary: %.2f\n", e.Name, e.Age, e.Positon, e.Salary)
+	fmt.Printf("Name: %s | Age: %d | Position: %s | Salary: %.2f\n", e.Name, e.Age, e.Position, e.Salary)
+}
+
+func addEmployee(employees []Employee, name string, age int16, position string, salary float64) []Employee{
+	newEmployee :=  Employee{
+		Name: name,
+		Age: age,
+		Position: position,
+		Salary: salary,
+	}
+	employees = append(employees, newEmployee)
+	return employees
+}
+
+func printAllEmployees(employees []Employee) {
+	fmt.Println("\n--- Список всех сотрудников ---")
+	for i, emp := range employees {
+		fmt.Printf("%d. Имя: %s, Возраст: %d, Должность: %s, Зарплата: %.2f\n",
+			i+1, emp.Name, emp.Age, emp.Position, emp.Salary)
+	}
+	fmt.Println()
 }
